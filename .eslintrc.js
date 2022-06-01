1a2c85e32ae21b71426136675f57147a5d9db8ab
Etienne <etienne.buronfosse@evaluo.eu>
2022-06-01 11:18:04 +0200

    try google versions eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
   },
-  extends: [
-    'standard'
+  'extends': [
+    'google',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
+  'parserOptions': {
+    'ecmaVersion': 'latest',
   },
-  rules: {
-  }
-}
+  'rules': {
+  },
+};
